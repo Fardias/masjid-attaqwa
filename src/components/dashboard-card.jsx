46f1ab3ddb1a7952e7f6ce37659e468516daa609
aftlah <altaf.fattah05@gmail.com>
2025-05-29 13:53:19 +0700

    feat: update banner masjid

--- src/components/dashboard-card.jsx
+++ src/components/dashboard-card.jsx
@@ -1,8 +1,20 @@
+'use client'
+
 import { Calendar, ImageIcon, Megaphone } from "lucide-react"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import Link from "next/link"
+import { getPengumuman } from "@/lib/services/PengumumanService"
+import { getUpcomingEvents } from "@/lib/services/eventService"
+import { getAllAlbums } from "@/lib/services/albumsService"
+import useSWR from "swr"
 
 export function DashboardCards() {
+    const fetcher = (fn) => fn()
+
+    const { data: countEvent = [] } = useSWR("upcoming-events", () => getUpcomingEvents(), { fetcher })
+    const { data: countPengumuman = [] } = useSWR("pengumuman", () => getPengumuman(), { fetcher })
+    const { data: countAlbum = [] } = useSWR("albums", () => getAllAlbums(), { fetcher })
+
     return (
         <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
             <Link href="/admin/dashboard/events">
@@ -12,8 +24,8 @@ export function DashboardCards() {
                         <Calendar className="w-5 h-5 text-amber-600" />
                     </CardHeader>
                     <CardContent>
-                        {/* <div className="text-3xl font-bold">12</div> */}
-                        {/* <p className="text-xs text-muted-foreground">3 event dalam 7 hari ke depan</p> */}
+                        <div className="text-3xl font-bold">{countEvent.length}</div>
+                        <p className="text-xs text-muted-foreground">3 event dalam 7 hari ke depan</p>
                     </CardContent>
                 </Card>
             </Link>
@@ -25,8 +37,8 @@ export function DashboardCards() {
                         <Megaphone className="w-5 h-5 text-amber-600" />
                     </CardHeader>
                     <CardContent>
-                        {/* <div className="text-3xl font-bold">8</div> */}
-                        {/* <p className="text-xs text-muted-foreground">2 pengumuman aktif saat ini</p> */}
+                        <div className="text-3xl font-bold">{countPengumuman.length}</div>
+                        <p className="text-xs text-muted-foreground">2 pengumuman aktif saat ini</p>
                     </CardContent>
                 </Card>
             </Link>
@@ -38,8 +50,8 @@ export function DashboardCards() {
                         <ImageIcon className="w-5 h-5 text-amber-600" />
                     </CardHeader>
                     <CardContent>
-                        {/* <div className="text-3xl font-bold">24</div> */}
-                        {/* <p className="text-xs text-muted-foreground">10 album kegiatan sosial, 14 album hari besar</p> */}
+                        <div className="text-3xl font-bold">{countAlbum.length}</div>
+                        <p className="text-xs text-muted-foreground">10 album kegiatan sosial, 14 album hari besar</p>
                     </CardContent>
                 </Card>
             </Link>
