c420a2dd9e3db618787d2bdf10520f8da6cd5874
Fardias <fardias_alf@gmail.com>
2025-06-17 21:19:39 +0700

    boost seo

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,6 @@
-export const siteUrl = 'https://masjidattaqwa-pbh.com';
-export const generateRobotsTxt = true;
-export const changefreq = 'weekly';
-export const priority = 0.7;
-  
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://masjidattaqwa-pbh.com',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+}
